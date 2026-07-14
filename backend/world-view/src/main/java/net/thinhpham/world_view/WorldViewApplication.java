fdc44f6486cb9790c44fd057883b844e8f58a181
thinhphamcs <phamtuongthinh@gmail.com>
2026-07-14 19:17:54 -0400

    chore: init Spring Boot backend skeleton with Maven

---
+++ backend/world-view/src/main/java/net/thinhpham/world_view/WorldViewApplication.java
@@ -0,0 +1,13 @@
+package net.thinhpham.world_view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldViewApplication.class, args);
+	}
+
+}
