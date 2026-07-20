1bb048c62587e72e18ee6beb2bb0f21d483b9cad
thinhphamcs <phamtuongthinh@gmail.com>
2026-07-20 19:57:20 -0400

    chore: added the new service block for kafka in the docker-compose.yml and fixed the relative path in the entrypoint.sh

--- kafka/kafka-entrypoint.sh
+++ kafka/kafka-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if kafka-storage.sh info -c ./server.properties; then
+if /opt/kafka/bin/kafka-storage.sh info -c ./server.properties; then
     echo "Kafka data directory exists. Skipping storage formatting."
 else
     echo "Kafka data directory does not exist. Formatting storage..."
-    kafka-storage format -t $(kafka-storage random-uuid) -c ./server.properties
+    /opt/kafka/bin/kafka-storage.sh format -t $(/opt/kafka/bin/kafka-storage.sh random-uuid) -c ./server.properties
 fi
-kafka-server-start.sh ./server.properties
+/opt/kafka/bin/kafka-server-start.sh ./server.properties
